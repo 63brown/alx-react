67a57a21d366e05349d34bbf8e3d85feb0d883b9
63brown <mutheemumbi01@gmail.com>
2023-08-01 15:15:29 +0300

    Task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, 'js/dashboard_main.js'),
+
+    output: {
+        path: path.resolve(__dirname, 'public' ),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|jpe?g|ico|gif)$/i,
+            use: [
+            {
+                loader: 'image-webpack-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images',
+                },
+            },
+            ],
+        },
+        ],
+    },
+};
